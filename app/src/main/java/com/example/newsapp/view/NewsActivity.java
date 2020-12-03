8bedf019f31544d76a706b7ff826911cee7ed1b6
aji2 <jajithjames@gmail.com>
2020-12-04 00:21:06 +0530

    second commit of news app

--- app/src/main/java/com/example/newsapp/view/NewsActivity.java
+++ app/src/main/java/com/example/newsapp/view/NewsActivity.java
@@ -146,4 +146,12 @@ public class NewsActivity extends DaggerAppCompatActivity implements NewsListAda
             }
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
 }
